3f6c71107e74655bd169aaa0d05536596ad2a4ca
rifgialhkm <rifgi115@gmail.com>
2023-03-22 08:35:54 +0700

    first commit

---
+++ checkName/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class FormCek
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Me.TextBox3.Text = Me.TextBox1.Text
+    End Sub
+End Class
